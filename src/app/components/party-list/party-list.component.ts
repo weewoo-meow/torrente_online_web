8cba35a4c392057379014f44a07353e730c2d5a8
David Martin <david.martin.melgar@gmail.com>
2025-06-17 18:05:47 +0200

    add: componente de listado de partida y ruta para el componentes tambien boton en nav de headers

---
+++ src/app/components/party-list/party-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-party-list',
+  imports: [],
+  templateUrl: './party-list.component.html',
+  styleUrl: './party-list.component.scss'
+})
+export class PartyListComponent {
+
+}
