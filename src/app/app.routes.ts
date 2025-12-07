32ea434ca756c25a27558019c645427eebcde094
srXendo <73126779+srXendo@users.noreply.github.com>
2025-12-07 18:12:35 +0100

    add:  apartado de dedicated_server. chg: binarios ahora con lfs

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { MatchmakerComponent } from './components/matchmaker/matchmaker.component';
 import { CommunityComponent } from './components/community/community.component';
 import { DownloadsComponent } from './components/downloads/downloads.component';
+import { DowloadsDedicatedComponent } from './components/dowloads-dedicated/dowloads-dedicated.component';
 
 export const routes: Routes = [
   { path: 'downloads', component: DownloadsComponent },
+  { path: 'downloads_dedicated', component: DowloadsDedicatedComponent },
   { path: 'matchmaker', component: MatchmakerComponent },
   { path: 'community', component: CommunityComponent },
 ];
